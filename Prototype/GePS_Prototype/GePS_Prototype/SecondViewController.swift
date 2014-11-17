f03619af4a934c0c7fbfe3c0f0038a74542d7024
Tyler Ward <btward@crimson.ua.edu>
2014-11-16 21:45:45 -0600

    added basic app layout

--- Prototype/GePS_Prototype/GePS_Prototype/SecondViewController.swift
+++ Prototype/GePS_Prototype/GePS_Prototype/SecondViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var addList: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,8 +21,8 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
 
 
 }
 
+}
