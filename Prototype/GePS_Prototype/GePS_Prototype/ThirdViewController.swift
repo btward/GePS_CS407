9e1fdce9b96e0cd03a7b4778946b6d530bd798db
Tyler Ward <btward@crimson.ua.edu>
2014-11-16 19:47:12 -0600

    cmt2

---
+++ Prototype/GePS_Prototype/GePS_Prototype/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  GePS_Prototype
+//
+//  Created by Tyler on 11/16/14.
+//
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
